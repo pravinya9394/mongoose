cc6bd74aabc1ee89832ce730065875b407010ad0
Pravinya Kumbhare <pravinyakumbhare@gmail.com>
2023-02-21 13:49:48 +0530

    Added schema and model part

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const mongoose =require("mongoose");
-mongoose.connect("mongodb://localhost:27017/pravinya",
-mongoose.set('strictQuery', false),
-()=>{
-    console.log("Connected to database successfully...")
-});
+
+// Connection creation to already existed db OR else create the new db
+mongoose.connect("mongodb://localhost:27017/pravinya",{useUrlParser:true, useUnifiedTopology:true})
+.then(()=>console.log("Connection Successfull.."))
+.catch((err)=>console.log(err)); 
+
+// Schema making
+const videoplayList=mongoose.Schema({
+    name:String,
+    type:Number,
+    video:Number,
+    active:Boolean
+})
