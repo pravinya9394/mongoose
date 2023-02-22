32ee8c7262a652cd40cc0954e6cd4840d57b8168
Pravinya Kumbhare <pravinyakumbhare@gmail.com>
2023-02-23 00:15:49 +0530

    Added schema and modeil

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
-const mongoose =require("mongoose");
+const mongoose=require("mongoose");
+mongoose.set('strictQuery', false);
+mongoose.connect(("mongodb://localhost:27017/Rupeek"))
+.then(()=>console.log("Connection Successfull..."))
+.catch((err) => console.log(err));
 
-// Connection creation to already existed db OR else create the new db
-mongoose.connect("mongodb://localhost:27017/pravinya",{useUrlParser:true, useUnifiedTopology:true})
-.then(()=>console.log("Connection Successfull.."))
-.catch((err)=>console.log(err)); 
-
-// Schema making
-const videoplayList=mongoose.Schema({
-    name:String,
-    type:Number,
-    video:Number,
-    active:Boolean
+const playlistSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    ctype:String,
+    videos:Number,
+    author:String,
+    active:Boolean,
+    date:{
+        type:Date,
+        default:Date.now
+    }
 })
+
+const Playlist=new mongoose.model("Playlist",playlistSchema);
+
+const createDocument=async()=>{
+    try{
+        const reactPlayList=new Playlist({
+            name:"ReactJs",
+            ctype:"Front end",
+            videos:80,
+            author:"Pravinya kumbhare",
+            active:true,
+        })
+        const result=await reactPlayList.save();
+        console.log(result);
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+// const nodePlayList=new Playlist({
+//     name:"NodeJs",
+//     ctype:"Back end",
+//     videos:100,
+//     author:"Pravinya kumbhare",
+//     active:true,
+// })
+
+createDocument();
